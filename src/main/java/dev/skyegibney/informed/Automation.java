84dba80c05f3c3475d0c5ec8264daf4cca1720ed
Skye Gibney <skyejgibney@gmail.com>
2022-12-19 14:53:41 -0800

    Fix level-2.html and change automation script

--- src/main/java/dev/skyegibney/informed/Automation.java
+++ src/main/java/dev/skyegibney/informed/Automation.java
@@ -4,13 +4,16 @@ import dev.skyegibney.informed.pages.*;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.io.File;
 
 public class Automation {
     public static void main(String[] args) {
         WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        WebDriver driver = new ChromeDriver(options);
 
         BaseLevel.inFormedPath = "file:///" + new File(".").getAbsolutePath() + "\\src\\main\\resources";
         BaseLevel.driver = driver;
@@ -28,29 +31,41 @@ public class Automation {
                 new Level10()
         };
 
-        for (var level: levels) {
-            System.out.print(level.getClass().getSimpleName());
+        int[] numberOfSuccesses = new int[levels.length];
+        float progress = 0;
+        int numRounds = 10;
 
-            try {
-                if (level.solve()) System.out.println(" OK");
-                else System.out.println(" Failed");
-            }
-            catch (Exception e) {
-                System.out.println(" Exception");
-                System.out.println(e.toString().split("\n")[0]);
+        for (int i = 0; i < numRounds; i++) {
+            for (int j = 0; j < levels.length; j++) {
+                try {
+                    if (levels[i].solve()) {
+                        numberOfSuccesses[i]++;
+                    }
+                } catch (Exception e) {
+                    System.out.println(" Exception");
+                    System.out.println(e.toString().split("\n")[0]);
+                }
+
+                progress += (float)1 / (levels.length * numRounds);
+                System.out.printf("%.2f\n", progress * 100);
             }
         }
 
+        for (int i = 0; i < levels.length; i++) {
+            System.out.println(levels[i].getClass().getSimpleName() + ": " + numberOfSuccesses[i] + " passed");
+        }
+
 //        Level2 level2 = new Level2();
-//        level2.solve();
+//        if (level2.solve()) System.out.println("PASS");
+//        else System.out.println("FAIL");
 //
 //        try {
-//            Thread.sleep(5000);
+//            Thread.sleep(10000);
 //        }
 //        catch (InterruptedException e) {
 //            e.printStackTrace();
 //        }
-
-        driver.quit();
+//
+//        driver.quit();
     }
 }
